0a5cd5bfdafbe453fc332d681ee7b070b5ab2a30
Ravindra Gudala <gudalaravindra2001@gmail.com>
2026-02-11 01:09:25 +0530

    Updated feature

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react"
+import type { Project } from "../types"
+import { dummyGenerations } from "../assets/assets"
+import { Loader } from "lucide-react"
+import ProjectCard from "../components/ProjectCard"
 
 
 const Community = () => {
-  return (
-    <div>Community</div>
+  const [projects, setProjects] = useState<Project[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchProjects = async () => {
+    setTimeout(() => {
+      setProjects(dummyGenerations.map(project => ({
+        ...project,
+        name: project.productName,
+        modelImageUrl: project.generatedImage,
+        uploadedImges: project.uploadedImages
+      })));
+      setLoading(false)
+    }, 3000)
+      
+  }
+
+  useEffect(() => {
+    fetchProjects()
+  },[])
+  
+  return loading ? (
+    <div className="min-h-screen flex items-center justify-center">
+      <Loader className="size-7 animate-spin text-indigo-400" />
+    </div>
+  ) : (   
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header className="mb-12">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">Community</h1>
+          <p className="text-gray-400">See what others are creating with our platform</p>
+        </header>
+        {/* Projects grid */}
+
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} gen={project} setGenerations={setProjects} forCommunity={true} />
+          ))}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Community
+export default Community 
--- src/pages/Genetator.tsx
+++ src/pages/Genetator.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
 import Title from "../components/Title"
 import UploadZone from "../components/UploadZone"
+import { Loader2Icon, RectangleHorizontalIcon, RectangleVerticalIcon, Wand2Icon } from "lucide-react"
+import { PrimaryButton } from "../components/Buttons"
 
  const Genetator = () => {
 
     const [name, setName] = useState('')
     const [productName, setProductName] = useState('')
     const [productDescription, setProductDescription] = useState('')
-    const [aspectRatio, setAspectRatio] = useState('1:1')
+    const [aspectRatio, setAspectRatio] = useState('9:16')
     const [productImage, setProductImage] = useState<File | null>(null)
     const [modelImage, setModelImage] = useState<File | null>(null)
     const [userPrompt, setUserPrompt] = useState('')
@@ -39,11 +41,53 @@ import UploadZone from "../components/UploadZone"
                     <UploadZone label="Product Image" file={productImage} onClear={() => setProductImage(null)} onChange={(e) => handleFileChange(e, 'product')} />
                     <UploadZone label="Model Image" file={modelImage} onClear={() => setModelImage(null)} onChange={(e) => handleFileChange(e, 'model')} />
                 </div>
-
-                <div>
-                    <p>Right col</p>
+                    {/* Right col */}
+                <div className="w-full">
+                    <div className="mb-4 text-gray-300">
+                        <label htmlFor="name" className="block text-sm mb-4">Project Name</label>
+                        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name your project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                    </div>
+                    <div className="mb-4 text-gray-300">
+                        <label htmlFor="productName" className="block text-sm mb-4">Product Name</label>
+                        <input type="text" id="productName" value={productName} onChange={(e) => setProductName(e.target.value)} placeholder="Enter name of the product" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
+                    </div>
+                    <div className="mb-4 text-gray-400">
+                        <label htmlFor="productDescription" className="block text-sm mb-4">Product Description <span className="text-xs text-violet-400">(optional)</span></label>
+                        <textarea id="productDescription" value={productDescription} rows={4} onChange={(e) => setProductDescription(e.target.value)} placeholder="Describe the product features, usage, and benefits" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all resize-none h-24"/>
+                    </div>
+                
+                    <div className="mb-4 text-gray-300">
+                        <label className="block text-sm mb-4">Aspect Ratio</label>
+                        <div className="flex gap-3">
+                            <RectangleVerticalIcon onClick={()=> setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '9:16' ? 'ring-violet-500/50 bg-white/10': ''}`}/>
+                            <RectangleHorizontalIcon onClick={()=> setAspectRatio('16:9')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '16:9' ? 'ring-violet-500/50 bg-white/10': ''}`}/>
+                        </div>
+                    </div>
+                    <div className="mb-4 text-gray-400">
+                        <label htmlFor="UserPrompt" className="block text-sm mb-4">User Prompt <span className="text-xs text-violet-400">(optional)</span></label>
+                        <textarea id="UserPrompt" value={userPrompt} rows={4} onChange={(e) => setUserPrompt(e.target.value)} placeholder="Describe what you want to generate for this product" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all resize-none h-24"/>
+                    </div>
                 </div>
             </div>
+
+            <div className="flex justify-center mt-10">
+                <PrimaryButton disabled={isGenerating} className="px-10 py-3 rounded-md disabled:opacity-70 disabled:cursor-not-allowed">
+                    {isGenerating ? (
+                        <>
+                        <Loader2Icon className="size-5 animate-spin"/>
+                        Generating...
+                        </>
+                    ) : (
+                        <>
+                           <Wand2Icon className="size-5"/>
+                           Generate Image
+                        </>
+                    )}
+                    
+                </PrimaryButton>
+
+            </div>
+
         </form>
      </div>
    )
